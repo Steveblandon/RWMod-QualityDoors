6bd4e278ef12e4e56fad95ddf6a0f7c009b2d654
Steveblandon <steveblandon@gmail.com>
2024-11-08 19:39:18 -0500

    updated to 1.5 and fixed health scaling bug

--- Source/QualityDoorsHarmonyPatch.cs
+++ Source/QualityDoorsHarmonyPatch.cs
@@ -10,31 +10,37 @@
         static QualityDoorsHarmonyPatch()
         {
             Harmony harmony = new Harmony("SteveZero.HarmonyPatch_QualityDoors");
+
             harmony.PatchAll();
-            harmony.Patch(AccessTools.Method(typeof(CompQuality), "SetQuality", null, null), null, new HarmonyMethod(typeof(QualityDoorsHarmonyPatch), "PatchSetQualityMethod", null), null, null);
-            harmony.Patch(AccessTools.DeclaredPropertyGetter(typeof(Thing), "MaxHitPoints"), null, new HarmonyMethod(typeof(QualityDoorsHarmonyPatch), "PatchMaxHitpointsProperty", null), null, null);
+
+            harmony.Patch(
+                AccessTools.Method(typeof(CompQuality), nameof(CompQuality.SetQuality)),
+                postfix: new HarmonyMethod(typeof(QualityDoorsHarmonyPatch), nameof(PatchSetQualityMethod)));
+
+            harmony.Patch(
+                AccessTools.DeclaredPropertyGetter(typeof(Thing), nameof(Thing.MaxHitPoints)),
+                postfix: new HarmonyMethod(typeof(QualityDoorsHarmonyPatch), nameof(PatchMaxHitpointsProperty)));
         }
 
-        public static void PatchSetQualityMethod(QualityCategory q, ArtGenerationContext source, CompQuality __instance)
+        public static void PatchSetQualityMethod(QualityCategory q, ArtGenerationContext? source, CompQuality __instance)
         {
             __instance.parent.HitPoints = __instance.parent.MaxHitPoints;
         }
 
         public static void PatchMaxHitpointsProperty(Thing __instance, ref int __result)
         {
-            bool isDoor = __instance.def.IsDoor;
-            if (isDoor)
+            if (__instance.def.IsDoor)
             {
                 CompQuality compQuality = ThingCompUtility.TryGetComp<CompQuality>(__instance);
-                bool flag = compQuality != null;
-                if (flag)
+
+                if (compQuality != null)
                 {
                     __result = (int)((float)__result * GetQualityFactor(compQuality.Quality));
                 }
             }
         }
 
-        public static float GetQualityFactor(QualityCategory q)
+        private static float GetQualityFactor(QualityCategory q)
         {
             float result;
             switch (q)
